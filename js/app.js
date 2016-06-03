1eba0abff861334878a2dada0ceaa676fcf621a3
wangmeng <wangmeng255@gmail.com>
2016-06-03 12:38:07 -0700

    contacts v1

--- js/app.js
+++ js/app.js
@@ -20,7 +20,21 @@ $(document).ready(function() {
 		$("#state").val("");
 
 		$(".list-contact ul").append(
-			"<li><a id=person" + String(persons.length-1) + " href='#0'>" + person.firstName + " " + person.lastName + "</a></li>"
+			"<li class=person><a href='#0'>" + person.firstName + " " + person.lastName + "</a></li>"
+		);
+	});
+	$(".list-contact").on("click", ".person", function() {
+		$(".show-contact ul").html("");
+		var listItem = $(this);
+		var i = $(this).index(".list-contact li");
+		$(".show-contact ul").prepend(
+			"<h2>" + persons[i].firstName + " " + persons[i].lastName + "</h2>"
+		);
+		$(".show-contact ul").append(
+			"<li>First name: " + persons[i].firstName + "</li>" +
+			"<li>Last name: " + persons[i].lastName + "</li>" +
+			"<li>Phone Number: " + persons[i].phoneNumber + "</li>" +
+			"<ul>Address:<li>" + persons[i].street + ", " + persons[i].city + ", " + persons[i].state + "</li></ul>"
 		);
 	});
 });
